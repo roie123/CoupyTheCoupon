17da2f4d8232d1365b41e2c72a3baf0560371e00
roie123 <roieivri123@gmail.com>
2023-04-26 19:45:07 +0300

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import NavBar from "./General Components/NavBar";
 import HomePage from "./Pages/HomePage";
 
 function App() {
